28ef0b8850f902bf4c4afb542b455a23e2242ff0
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-04-29 18:37:45 -0300

    Posible solución de invariants

--- ruby/lib/before_and_after.rb
+++ ruby/lib/before_and_after.rb
@@ -1,30 +1,36 @@
+require_relative 'invariants'
+
 module AntesYDespues
     def before_and_after_each_call(before_proc, after_proc)
+        # Inicializo los arrays si no existen
         @before_procs ||= []
         @after_procs ||= []
 
+        # Añado los procs al final de las listas (solo si no son nil)
         @before_procs << before_proc
         @after_procs << after_proc
     end
 
     def method_added(method_name)
-        return if @mato_recursividad # jonkler func
+        return if @method_added # Evito recursión infinita ya que define_method llama a method_added
+        @method_added = true
 
-        metodo = instance_method(method_name)
+        metodo_original = self.instance_method(method_name) # Obtengo el método original (unbound method)
 
-        @mato_recursividad = true
-
-        define_method(method_name) do |*args, &block|
-            self.class.instance_variable_get(:@before_procs)&.each do |before_proc|
-                before_proc.call
+        define_method(method_name) do |*args, &block| # Redefino la lógica del método
+            self.class.instance_variable_get(:@before_procs)&.each do |before_proc| # Primero se ejecutan los before_procs
+                before_proc.call(self,args) # Nuevo
             end
-            retorno = metodo.bind(self).call(*args, &block) # Guardo el valor de retorno
-            self.class.instance_variable_get(:@after_procs)&.each do |after_proc|
-                after_proc.call
+
+            retorno = metodo_original.bind(self).call(*args, &block) # En el medio se ejecuta el método original (primero lo bindeo a self)
+
+            self.class.instance_variable_get(:@after_procs)&.each do |after_proc| # Luego se ejecutan los after_procs
+                after_proc.call(self,args) # Nuevo
             end
+
             retorno
         end
-
-        @mato_recursividad = false
+        @method_added = false
     end
+
 end
--- ruby/lib/invariants.rb
+++ ruby/lib/invariants.rb
@@ -10,3 +10,21 @@ module Invariante
         before_and_after_each_call(proc {}, proc_invariante)
     end
 end
+
+=begin
+module Invariante
+    def invariant(&block)
+        @invariant_block = block
+
+        wrapper = proc do |instance|
+            instance.instance_eval do
+                unless instance_eval(&block)
+                    raise "Excepción: estado inválido"
+                end
+            end
+        end
+        before_and_after_each_call(proc{}, wrapper)
+    end
+end
+=end
+# TODO: Chequear que funcione
